b6cbb90fccc38dc30bb9688c2d4762c51ee17200
Alexander Brown <alexandermichaelbrown@outlook.com>
2023-06-04 17:58:34 +1000

    Begin work on adaptive

--- src/intersect/adaptive.rs
+++ src/intersect/adaptive.rs
@@ -1,5 +1,7 @@
 /// Adaptive set intersection algorithms.
 
+use smallvec::{SmallVec, smallvec};
+
 use crate::{
     intersect::search::binary_search,
     visitor::Visitor,
@@ -39,4 +41,69 @@ where
 
     baezayates(&small_set[small_partition+1..],
                        &large_set[large_partition..], visitor)
+}
+
+pub fn adaptive<T, S, V>(sets: &[S], visitor: &mut V)
+where
+    T: Ord + Copy,
+    S: AsRef<[T]>,
+    V: Visitor<T>,
+{
+    assert!(sets.len() >= 2);
+
+    let mut positions_vec: SmallVec<[usize; 8]> = smallvec![0; sets.len()];
+    let positions = &mut positions_vec[..];
+
+    let mut elim_set_idx = 0;
+    let mut elim_value = sets[0].as_ref()[0];
+    let mut curr_set_idx = 1;
+    let mut gallop_size = 1;
+    
+    loop {
+        let elim_set = sets[elim_set_idx].as_ref();
+        let curr_set = sets[curr_set_idx].as_ref();
+        let curr_position = positions[curr_set_idx];
+
+        if curr_set[curr_position + gallop_size] >= elim_value {
+            let search_result = binary_search(
+                curr_set, elim_value, curr_position, curr_position + gallop_size);
+
+            positions[curr_set_idx] = search_result;
+            if curr_set[search_result] == elim_value {
+                // Found
+                positions[curr_set_idx] += 1;
+                curr_set_idx = (curr_set_idx + 1) % sets.len();
+
+                if curr_set_idx == elim_set_idx {
+                    // Update eliminator
+                    visitor.visit(elim_value);
+
+                    if search_result == curr_set.len() - 1 {
+                        break;
+                    }
+                    elim_set_idx += 1;
+                    elim_value = elim_set[elim_set_idx];
+
+                    curr_set_idx = (curr_set_idx + 1) % sets.len();
+                }
+            }
+            else {
+                // Not found
+                // TODO
+            }
+            
+
+        }
+        else if curr_set[curr_set.len()-1] < elim_value {
+            break;
+        }
+        
+        // Update gallop size
+        gallop_size = if positions[curr_set_idx] + gallop_size*2 < curr_set.len() {
+            gallop_size * 2
+        }
+        else {
+            curr_set.len() - positions[curr_set_idx] - 1
+        }
+    }
 }
