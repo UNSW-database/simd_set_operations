a75335488df4566316cd84c424cb64f72d53a36a
Alex Brown <alexbr-@outlook.com>
2023-07-25 23:11:22 +1000

    Plan plot executable

--- benchmark/src/bin/plot.rs
+++ benchmark/src/bin/plot.rs
@@ -1,8 +1,16 @@
+use clap::Parser;
+use std::path::PathBuf;
 
-
-
-
-fn main() {
-
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(default_value = "experiment.toml")]
+    experiment: PathBuf,
+    #[arg(default_value = "results.json")]
+    out: PathBuf,
 }
 
+fn main() {
+    // Load results
+    // Load experiment.toml
+}
