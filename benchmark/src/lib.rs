e59c8a14bc2e6237d89146dd875fe14701504d5e
Alexander Brown <alexandermichaelbrown@outlook.com>
2023-07-28 12:10:49 +1000

    Remove old generator

--- benchmark/src/lib.rs
+++ benchmark/src/lib.rs
@@ -2,32 +2,8 @@ pub mod schema;
 pub mod generators;
 pub mod datafile;
 
-use std::{collections::BTreeSet, ops::{Range, RangeInclusive}, path::PathBuf, iter::StepBy};
-use rand::{distributions::Uniform, prelude::Distribution, seq::SliceRandom, thread_rng};
+use std::{ops::RangeInclusive, path::PathBuf, iter::StepBy};
 use schema::{TwoSetDatasetInfo, Parameter};
-
-#[deprecated]
-pub fn uniform_sorted_set(range: Range<i32>, cardinality: usize) -> Vec<i32> {
-    let rng = &mut thread_rng();
-
-    let density = cardinality as f64 / range.len() as f64;
-    if density < 0.01 {
-        let dist = Uniform::from(range);
-
-        let mut set: BTreeSet<i32> = BTreeSet::new();
-        while set.len() < cardinality {
-            set.insert(dist.sample(rng));
-        }
-        set.iter().copied().collect()
-    } else {
-        let mut everything: Vec<i32> = range.collect();
-        everything.shuffle(rng);
-
-        let mut result = Vec::from(&everything[0..cardinality]);
-        result.sort();
-        result
-    }
-}
 
 pub fn fmt_open_err(e: impl ToString, path: &PathBuf) -> String {
     format!("unable to open {}: {}", path_str(path), e.to_string())
