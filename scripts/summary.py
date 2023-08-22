a961baab8e1b450fcf60c0c4e4cf2d1b438525f5
Alexander Brown <alexandermichaelbrown@outlook.com>
2023-08-22 15:48:40 +1000

    Add datasets to summary.py

--- scripts/summary.py
+++ scripts/summary.py
@@ -10,12 +10,61 @@ results = json.loads(results_file.read())
 
 os.makedirs("plots", exist_ok=True)
 
+def write_experiment(experiment):
+    title = experiment["title"]
+    name = experiment["name"]
+    with tag("h2"):
+        text(title)
+    with tag("div"):
+        with tag("i"):
+            text(name)
+    doc.stag("img", src=f"{name}.svg")
+
+def write_field(name, value):
+    with tag("div"):
+        with tag("b"):
+            text(name + ": ")
+        with tag("span"):
+            text(value)
+
+def write_dataset(dataset):
+    name = dataset["name"]
+    dataset_type = dataset["type"]
+    with tag("h2"):
+        text(name)
+    
+    write_field("type", dataset_type)
+    if dataset_type == "synthetic":
+        write_field("vary", dataset["vary"])
+        write_field("to", dataset["to"])
+        write_field("step", dataset["step"])
+        write_field("gen_count", dataset["gen_count"])
+
+        set_count = dataset["set_count"]
+        write_field("set_count", set_count)
+        write_field("density", dataset["density"] / 1000)
+        write_field("selectivity", dataset["selectivity"] / 1000)
+        
+        max_len = dataset["max_len"]
+        max_len_count = 2 ** max_len
+        write_field("max_len", f"2^{max_len} elements ({max_len_count})")
+        write_field("skewness_factor", dataset["skewness_factor"])
+    else:
+        write_field("gen_count", dataset["gen_count"])
+        write_field("set_count_start", dataset["set_count_start"])
+        write_field("set_count_end", dataset["set_count_end"])
+
 with tag("html"):
     with tag("body"):
+        with tag("h1"):
+            text("Experiments")
+        
         for experiment in results["experiments"]:
-            name = experiment["name"]
-            with tag("h1"):
-                text(name)
-            doc.stag("img", src=f"{name}.svg")
+            write_experiment(experiment)
+        
+        with tag("h1"):
+            text("Datasets")
+        for dataset in results["datasets"].values():
+            write_dataset(dataset["info"])
 
 print(doc.getvalue())
