242da0637599248edb09071080458a8ac3207faa
Alex Brown <alexbr-@outlook.com>
2023-08-28 11:05:52 +1000

    Improve comparison plotting

--- scripts/compare.py
+++ scripts/compare.py
@@ -8,9 +8,9 @@ import plot
 
 def plot_experiment(experiment, results, reference):
     results_times, info = process_results(experiment, results)
-    reference_times, info = process_results(experiment, results)
+    reference_times, info = process_results(experiment, reference)
 
-    reference_times = { k + "_old": v for k, v in reference_times.items() }
+    # reference_times = { k + "_old": v for k, v in reference_times.items() }
 
     results_df = pd.DataFrame(
         results_times,
@@ -20,7 +20,7 @@ def plot_experiment(experiment, results, reference):
         reference_times,
         index=[*plot.get_vary_range(info)])
 
-    return plot_comparison_absolute(results_df, reference_df, info)
+    return plot_comparison(results_df, reference_df, info, experiment["relative_to"])
 
 def process_results(experiment, results):
     algorithms = results["algorithm_sets"][experiment["algorithm_set"]]
@@ -36,23 +36,39 @@ def process_results(experiment, results):
     
     return (times, info)
 
-def plot_comparison_absolute(results_df, reference_df, info):
-    fig, ax = plt.subplots()
+def plot_comparison(results_df, reference_df, info, relative_to):
+    results_df = make_relative(results_df, relative_to)
+    relative_df = make_relative(reference_df, relative_to)
 
-    ax.plot(results_df, linestyle="solid")
-    ax.plot(reference_df, linestyle="dashed")
+    ax = results_df.plot(linestyle="solid")
+
+    plt.gca().set_prop_cycle(None)
+
+    relative_df.plot(ax=ax, linestyle="dotted")
     
     ax.set_xlabel(plot.format_xlabel(info))
-    ax.set_ylabel("intersection time")
+    ax.set_ylabel(f"relative speedup ({relative_to})")
 
     if plot.use_log(info):
         ax.set_yscale("log")
 
     ax.xaxis.set_major_formatter(lambda x, _: plot.format_x(x, info))
-    ax.yaxis.set_major_formatter(lambda y, _: plot.format_time(y))
+    # ax.yaxis.set_major_formatter(lambda y, _: plot.format_time(y))
     ax.grid()
+    ax.legend()
 
-    return fig
+    return ax.get_figure()
+
+def make_relative(df, relative_to):
+    base = df[relative_to]
+
+    speedup_absolute = -df.sub(base, axis="index")
+    speedup_relative = speedup_absolute.div(base, axis="index")
+
+    speedup_relative.drop(columns=[relative_to])
+
+    return speedup_relative
+
 
 def main():
     results_file   = open("results.json", "r")
