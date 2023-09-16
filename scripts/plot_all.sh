e3823fa53b438cae5e882e421ea24270495733aa
Alex Brown <alexbr-@outlook.com>
2023-09-16 10:37:49 +1000

    plot_all: allow different results path

--- scripts/plot_all.sh
+++ scripts/plot_all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-python3 scripts/plot.py &&
-python3 scripts/summary.py > plots/index.html &&
+python3 scripts/plot.py $1 &&
+python3 scripts/summary.py $1 > plots/index.html &&
 echo plots/index.html
