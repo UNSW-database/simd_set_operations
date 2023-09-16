ecc91f6f3eacf4fb5e08b7e1705b7f7a97432eda
Alex Brown <alexbr-@outlook.com>
2023-09-16 10:38:22 +1000

    Restrict croaring CPU arch

--- scripts/bench_all.sh
+++ scripts/bench_all.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 run_bench() {
-    RUSTFLAGS="-C target-cpu=native $1" cargo run --release --bin=benchmark -- \
+    RUSTFLAGS="-C target-cpu=$1" ROARING_ARCH="$1" cargo run --release --bin=benchmark -- \
         --out=$2\
         # --datasets=/data/alexb/datasets
 }
 
-run_bench "-C target-feature=-avx2 -C target-feature=-avx512f" "results-sse.json"
-run_bench "-C target-feature=-avx512f" "results-avx2.json"
-run_bench "" "results-avx512.json"
+run_bench "nehalem" "results-sse.json"
+run_bench "broadwell" "results-avx2.json"
+run_bench "native" "results-avx512.json"
