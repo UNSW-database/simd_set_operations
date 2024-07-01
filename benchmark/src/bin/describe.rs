a8735b9b18a2aebd50a7db49464159a392a27337
William Miles <wmpmiles@gmail.com>
2024-07-01 20:14:16 +1000

    Updated describe with k-set generation. Basic sanity check, no tests.

--- benchmark/src/bin/describe.rs
+++ benchmark/src/bin/describe.rs
@@ -1,30 +1,56 @@
-use benchmark::{fmt_open_err, path_str, DataBinConfig, Datatype, Distribution};
+use benchmark::{
+    fmt_open_err, path_str, util::sample_distribution_unique, DataBinConfig, DataBinLengths,
+    DataDescription, DataDistribution, Datatype,
+};
 use clap::Parser;
 use colored::*;
 use std::{
     collections::HashMap,
     fs::{self, File},
+    iter,
     path::PathBuf,
 };
 
 use rand::{Rng, SeedableRng};
 use serde::Deserialize;
+use zipf::ZipfDistribution;
 
 // Schema for TOML configuration file
 #[derive(Deserialize, Debug)]
 struct Config {
-    pub dataset: HashMap<String, Dataset>,
+    dataset: HashMap<String, Dataset>,
 }
 
 #[derive(Deserialize, Debug)]
 struct Dataset {
-    pub datatype: VecParamOpt<Datatype>,
-    pub max_length: NumParamOpt<usize>,
-    pub trials: NumParamOpt<u64>,
-    pub selectivity: NumParamOpt<f64>,
-    pub skew: NumParamOpt<f64>,
-    pub density: NumParamOpt<f64>,
-    pub distribution: VecParamOpt<Distribution>,
+    datatype: VecParamOpt<Datatype>,
+    max_length: NumParamOpt<usize>,
+    trials: NumParamOpt<usize>,
+    selectivity: NumParamOpt<f64>,
+    skew: NumParamOpt<f64>,
+    density: NumParamOpt<f64>,
+    distribution: VecParamOpt<DataDistribution>,
+    #[serde(flatten)]
+    kset_info: Option<KSetInfo>,
+}
+
+#[derive(Deserialize, Debug)]
+struct KSetInfo {
+    sets: NumParamOpt<u64>,
+    corpus: Corpus,
+}
+
+#[derive(Deserialize, Debug)]
+struct Corpus {
+    size: NumParamOpt<u64>,
+    distribution: VecParamOpt<CorpusDistribution>,
+    samples: NumParamOpt<usize>,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Clone, Copy)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum CorpusDistribution {
+    Zipf {},
 }
 
 type VecParamOpt<T> = OptParameter<T, Vec<T>>;
@@ -116,11 +142,18 @@ fn generate(cli: &Cli) -> Result<(), String> {
         validate_param_normalized(&dataset.selectivity, "selectivity")?;
         validate_param_normalized(&dataset.skew, "skew")?;
         validate_param_normalized(&dataset.density, "density")?;
+        if let Some(kset_info) = &dataset.kset_info {
+            validate_param_unsigned(&kset_info.sets, "sets")?;
+            validate_param_unsigned(&kset_info.corpus.size, "corpus_size")?;
+            validate_param_unsigned(&kset_info.corpus.samples, "samples")?;
+        }
 
         // Generate data bin configurations
-        let data_bins: Vec<DataBinConfig> = {
+        let data_bins: DataDescription = {
             let mut ret = Vec::<DataBinConfig>::new();
 
+            // Could be done with `iproduct!` or chained `.flat_map`, but they both add un-needed complexity to
+            // something that would ideally be solved with a relatively simple macro.
             let mut offset = 0usize;
             for datatype in dataset.datatype.param_range() {
                 for distribution in dataset.distribution.param_range() {
@@ -129,18 +162,55 @@ fn generate(cli: &Cli) -> Result<(), String> {
                             for density in dataset.density.param_range() {
                                 for skew in dataset.skew.param_range() {
                                     for selectivity in dataset.selectivity.param_range() {
-                                        let seed: u64 = rng.gen();
-                                        ret.push(statistics_to_description(
-                                            datatype,
-                                            max_length,
-                                            trials,
-                                            selectivity,
-                                            skew,
-                                            density,
-                                            distribution,
-                                            seed,
-                                            &mut offset,
-                                        )?);
+                                        // Split between 2-set and k-set logic
+                                        if let Some(kset_info) = &dataset.kset_info {
+                                            for sets in kset_info.sets.param_range() {
+                                                for corpus_size in
+                                                    kset_info.corpus.size.param_range()
+                                                {
+                                                    for corpus_dist in
+                                                        kset_info.corpus.distribution.param_range()
+                                                    {
+                                                        for length_samples in
+                                                            kset_info.corpus.samples.param_range()
+                                                        {
+                                                            let seed: u64 = rng.gen();
+                                                            ret.push(
+                                                                statistics_to_description_kset(
+                                                                    datatype,
+                                                                    max_length,
+                                                                    trials,
+                                                                    selectivity,
+                                                                    skew,
+                                                                    density,
+                                                                    distribution,
+                                                                    seed,
+                                                                    &mut offset,
+                                                                    sets,
+                                                                    corpus_size,
+                                                                    corpus_dist,
+                                                                    length_samples,
+                                                                    &mut rng,
+                                                                )?,
+                                                            );
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        } else {
+                                            let seed: u64 = rng.gen();
+                                            ret.push(statistics_to_description_2set(
+                                                datatype,
+                                                max_length,
+                                                trials,
+                                                selectivity,
+                                                skew,
+                                                density,
+                                                distribution,
+                                                seed,
+                                                &mut offset,
+                                            )?);
+                                        }
                                     }
                                 }
                             }
@@ -169,7 +239,7 @@ fn generate(cli: &Cli) -> Result<(), String> {
 
 fn validate_param_unsigned<T>(param: &NumParamOpt<T>, name: &str) -> Result<(), String>
 where
-    T: From<u8> + PartialEq
+    T: From<u8> + PartialEq,
 {
     match param {
         OptParameter::Fixed(v) => {
@@ -280,9 +350,10 @@ impl ParamRange<f64> for NumParamOpt<f64> {
 impl ParamRange<u64> for NumParamOpt<u64> {
     fn param_range(&self) -> Vec<u64> {
         param_range_opt(self, |p| {
-            param_range_num(p).iter().map(|v| unsafe {
-                v.round().to_int_unchecked()
-            }).collect()
+            param_range_num(p)
+                .iter()
+                .map(|v| unsafe { v.round().to_int_unchecked() })
+                .collect()
         })
     }
 }
@@ -290,9 +361,10 @@ impl ParamRange<u64> for NumParamOpt<u64> {
 impl ParamRange<usize> for NumParamOpt<usize> {
     fn param_range(&self) -> Vec<usize> {
         param_range_opt(self, |p| {
-            param_range_num(p).iter().map(|v| unsafe {
-                v.round().to_int_unchecked()
-            }).collect()
+            param_range_num(p)
+                .iter()
+                .map(|v| unsafe { v.round().to_int_unchecked() })
+                .collect()
         })
     }
 }
@@ -338,45 +410,135 @@ fn param_range_num(p: &NumericalParameter) -> Vec<f64> {
         .collect()
 }
 
-fn statistics_to_description(
+fn statistics_to_description_2set(
     datatype: Datatype,
     max_length: usize,
-    trials: u64,
+    trials: usize,
     selectivity: f64,
     skew: f64,
     density: f64,
-    distribution: Distribution,
+    distribution: DataDistribution,
     seed: u64,
     offset: &mut usize,
 ) -> Result<DataBinConfig, String> {
     let long_length = max_length;
     let short_length = (max_length as f64 * skew) as usize;
+    let set_lengths = vec![long_length, short_length];
     let intersection_length = (short_length as f64 * selectivity) as usize;
+    let lengths = vec![ DataBinLengths { set_lengths, intersection_length } ];
 
     let max_value = (long_length as f64 / density).min(datatype.max() as f64) as u64;
 
     let bin = DataBinConfig {
         datatype,
         max_value,
-        long_length,
-        short_length,
+        lengths,
         trials,
-        intersection_length,
         distribution,
         seed,
         offset: *offset,
     };
 
-    // Sanity check sizes
-    let minimum_cardinality = (long_length - intersection_length) + short_length;
-    if (max_value as usize) < minimum_cardinality {
+    *offset += (long_length + short_length + intersection_length) * datatype.bytes() * trials;
+
+    Ok(bin)
+}
+
+fn statistics_to_description_kset(
+    datatype: Datatype,
+    max_length: usize,
+    trials: usize,
+    selectivity: f64,
+    skew: f64,
+    density: f64,
+    distribution: DataDistribution,
+    seed: u64,
+    offset: &mut usize,
+    set_count: u64,
+    corpus_size: u64,
+    corpus_dist: CorpusDistribution,
+    length_samples: usize,
+    rng: &mut impl Rng,
+) -> Result<DataBinConfig, String> {
+    // Sanity check to avoid infinite loop when sampling distribution
+    if set_count > corpus_size {
         return Err(format!(
-            "Density ({}) too high for given selectivity ({}).\n{:#?}",
-            density, selectivity, bin
+            "Corpus size ({}) must be larger than set count ({}).",
+            corpus_size, set_count
         ));
     }
 
-    *offset += (long_length + short_length + intersection_length) * datatype.bytes(); 
+    let lengths: Vec<DataBinLengths> = if skew == 1.0 {
+        iter::repeat(DataBinLengths {
+            set_lengths: iter::repeat(max_length).take(set_count as usize).collect(),
+            intersection_length: (max_length as f64 * selectivity) as usize,
+        })
+        .take(length_samples)
+        .collect()
+    } else {
+        match corpus_dist {
+            CorpusDistribution::Zipf {} => {
+                let zipf_exponent = -f64::log(skew, corpus_size as f64);
+                let length_dist = match ZipfDistribution::new(corpus_size as usize, zipf_exponent) {
+                        Ok(x) => Ok(x),
+                        Err(()) => Err(format!(
+                            "Failed to create discrete Zipf distribution with (num_elements = {}, exponent = {}).", 
+                            corpus_size as usize, zipf_exponent
+                        )),
+                    }?;
+                iter::repeat_with(|| {
+                    let mut set_index: Vec<usize> = if set_count == corpus_size {
+                        (1..=(set_count as usize)).collect()
+                    } else {
+                        sample_distribution_unique(set_count as usize, &length_dist, rng)
+                    };
+                    set_index.sort_unstable();
+                    let set_lengths: Vec<usize> = set_index
+                        .into_iter()
+                        .map(|i| {
+                            (max_length as f64 * (1.0 / (i as f64).powf(zipf_exponent))).round()
+                                as usize
+                        })
+                        .collect();
+
+                    let shortest_length = *set_lengths.last().unwrap();
+                    let intersection_length = (shortest_length as f64 * selectivity) as usize;
+
+                    DataBinLengths {
+                        set_lengths,
+                        intersection_length,
+                    }
+                })
+                .take(length_samples)
+                .collect()
+            }
+        }
+    };
+
+    let offset_delta = {
+        let mut delta = 0usize;
+        for databin_lengths in &lengths {
+            let total_set_length = databin_lengths.set_lengths.iter().fold(0usize, |acc, x| acc + *x);
+            let total_length = total_set_length + databin_lengths.intersection_length;
+            let total_bytes = total_length * datatype.bytes() * trials;
+            delta += total_bytes;
+        }
+        delta
+    };
+
+    let max_value = (max_length as f64 / density).min(datatype.max() as f64) as u64;
+
+    let bin = DataBinConfig {
+        datatype,
+        max_value,
+        lengths,
+        trials,
+        distribution,
+        seed,
+        offset: *offset,
+    };
+
+    *offset += offset_delta;
 
     Ok(bin)
 }
--- benchmark/src/lib.rs
+++ benchmark/src/lib.rs
@@ -1,12 +1,13 @@
 #![feature(portable_simd)]
 
-pub mod datafile;
-pub mod format;
-pub mod generators;
-pub mod schema;
+// pub mod datafile;
+// pub mod format;
+// pub mod generators;
+// pub mod schema;
 // pub mod timer;
-pub mod realdata;
+// pub mod realdata;
 pub mod util;
+pub mod algorithms;
 
 use std::path::PathBuf;
 
@@ -48,26 +49,33 @@ impl Datatype {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Copy)]
-#[serde(tag = "type", rename_all = "snake_case")]
-pub enum Distribution {
-    Uniform {},
-}
+pub type DataDescription = Vec<DataBinConfig>;
 
 // Data bin configuration
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct DataBinConfig {
     pub datatype: Datatype,
     pub max_value: u64,
-    pub long_length: usize,
-    pub short_length: usize,
-    pub intersection_length: usize,
-    pub distribution: Distribution,
+    pub lengths: Vec<DataBinLengths>,
+    pub distribution: DataDistribution,
     pub seed: u64,
-    pub trials: u64,
+    pub trials: usize,
     // byte offset in .data file
     pub offset: usize,
 }
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct DataBinLengths {
+    pub set_lengths: Vec<usize>,
+    pub intersection_length: usize
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Copy)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum DataDistribution {
+    Uniform {},
+}
+
 
 /*
 pub fn props_at_x(info: &SyntheticDataset, x: u32) -> IntersectionInfo {
