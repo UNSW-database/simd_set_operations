8959c81023543f68c0ed0024f340a730221cf172
Alex Brown <alexandermichaelbrown@outlook.com>
2024-07-03 02:49:00 +0000

    asm gen: add branch algorithms
    Signed-off-by: Alex Brown <alexandermichaelbrown@outlook.com>

--- scripts/results/asm.sh
+++ scripts/results/asm.sh
@@ -3,7 +3,8 @@
 OUT_DIR="${1:-asm}"
 CPU="${2:-native}"
 
-for algo in bmiss bmiss_sttni branchless_merge broadcast_avx2 broadcast_avx512 broadcast_sse naive_merge qfilter_c qfilter shuffling_avx2 shuffling_avx512 shuffling_sse vp2intersect_emulation
+
+for algo in bmiss_branch_mono bmiss_mono bmiss_sttni_branch_mono bmiss_sttni_mono branchless_merge_mono broadcast_avx2_branch_mono broadcast_avx2_mono broadcast_avx512_branch_mono broadcast_avx512_mono broadcast_sse_branch_mono broadcast_sse_mono naive_merge_mono qfilter_branch_mono qfilter_c_mono qfilter_mono shuffling_avx2_branch_mono shuffling_avx2_mono shuffling_avx512_branch_mono shuffling_avx512_mono shuffling_sse_branch_mono shuffling_sse_mono vp2intersect_emulation_branch_mono vp2intersect_emulation_mono
 do
     mkdir -p "$OUT_DIR"
     OUT_FILE="$OUT_DIR/$algo.asm"
