6583d7051d4d5703a48b819cfbc5f8a7f82c51a2
Alex Brown <alexandermichaelbrown@outlook.com>
2024-07-10 04:38:05 +0000

    Restructure asm generation script
    Signed-off-by: Alex Brown <alexandermichaelbrown@outlook.com>

--- scripts/results/asm.sh
+++ scripts/results/asm.sh
@@ -2,22 +2,37 @@
 
 OUT_DIR="${1:-asm}"
 CPU="${2:-native}"
+MCA_ARGS="${3}"
 
+asm() {
+    ASM_FILE="$OUT_DIR/$1.asm"
+    RUSTFLAGS="-C target-cpu=$CPU" cargo asm \
+        --release --package=setops --lib \
+        --intel --simplify "$1"_mono > "$ASM_FILE" 2> /dev/null
+    echo "$ASM_FILE"
+}
 
-for algo in bmiss_branch bmiss bmiss_sttni_branch bmiss_sttni branchless_merge broadcast_avx2_branch broadcast_avx2 broadcast_avx512_branch broadcast_avx512 broadcast_sse_branch broadcast_sse naive_merge qfilter_branch qfilter shuffling_avx2_branch shuffling_avx2 shuffling_avx512_branch shuffling_avx512 shuffling_sse_branch shuffling_sse vp2intersect_emulation_branch vp2intersect_emulation
+mca() {
+    MCA_FILE="$OUT_DIR/$1.mca"
+    RUSTFLAGS="-C target-cpu=$CPU" cargo asm \
+        --release --package=setops --lib \
+        --mca-intel "$1"_mono $MCA_ARGS > "$MCA_FILE" 2> /dev/null
+    echo "$MCA_FILE"
+}
+
+# for algo in bmiss bmiss_sttni branchless_merge broadcast_avx2 broadcast_avx512 broadcast_sse naive_merge qfilter shuffling_avx2 shuffling_avx512 shuffling_sse vp2intersect_emulation
+for algo in bmiss bmiss_sttni branchless_merge broadcast_avx2 broadcast_sse naive_merge qfilter shuffling_avx2 shuffling_sse
 do
     mkdir -p "$OUT_DIR"
-    ASM_FILE="$OUT_DIR/$algo.asm"
-    MCA_FILE="$OUT_DIR/$algo.mca"
 
-    RUSTFLAGS="-C target-cpu=$CPU" cargo asm \
-        --release --package=setops --lib \
-        --intel --simplify "$algo"_mono > "$ASM_FILE" 2> /dev/null
-    echo "$ASM_FILE"
+    asm ${algo}_lut
+    asm ${algo}_br_lut
+    # asm ${algo}_comp
+    # asm ${algo}_br_comp
 
-    RUSTFLAGS="-C target-cpu=$CPU" cargo asm \
-        --release --package=setops --lib \
-        --mca-intel "$algo"_mono > "$MCA_FILE" 2> /dev/null
-    echo "$MCA_FILE"
+    mca ${algo}_lut
+    mca ${algo}_br_lut
+    # mca ${algo}_comp
+    # mca ${algo}_br_comp
 done
 
