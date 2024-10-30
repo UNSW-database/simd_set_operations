aa5d9cac31914a1272a2f7af71cac8dfe6be63d3
William Miles <wmpmiles@gmail.com>
2024-10-30 12:59:08 +1100

    Expand plot_1d - Add note support - Add graphs of absolute values - Add graphs for more counters - Use stored databin index rather than implicit index from results - Change graphs to use compact layout

--- scripts/plot_1d.py
+++ scripts/plot_1d.py
@@ -15,6 +15,19 @@ MS = 1_000
 
 VARIABLES = {"skew", "density", "selectivity", "size", "datatype", "distribution"}
 
+CNAMES = ('cycles', 'cache_misses', 'branch_misses')
+PROPORTIONS = (
+    lambda val, ref: np.average(ref) / np.average(val),
+    lambda val, ref: np.average(val) / np.average(ref),
+    lambda val, ref: np.average(val) / np.average(ref),
+)
+YAXIS = (
+    'Speedup (ref. value / value)', 
+    'Ratio (value / ref. value)',
+    'Ratio (value / ref. value)',
+)
+
+
 def main():
     parser = argparse.ArgumentParser(
         description = "Graphs relative performance of algorithms for a single variable."
@@ -24,8 +37,8 @@ def main():
     parser.add_argument("variable", help="Variable to graph.")
     parser.add_argument("reference", help="Reference algorithm.")
     parser.add_argument("-rw", default=0, type=int, help="Remove this many values from the start of each trial.")
-    parser.add_argument("-x", "--width", default=10, type=int, help="Image width in inches.")
-    parser.add_argument("-y", "--height", default=10, type=int, help="Image height in inches.")
+    parser.add_argument("-x", "--width", default=10, type=float, help="Image width in inches.")
+    parser.add_argument("-y", "--height", default=10, type=float, help="Image height in inches.")
 
     args = parser.parse_args()
 
@@ -43,13 +56,10 @@ def main():
     with open(description_path, "r") as description_file:
         description = json.load(description_file)
 
+    note = f"Note: {'' if not 'note' in results else results['note']}"
+
     # We output the graphs to the same directory as the input results file
     os.chdir(results_path.parents[0])
-
-    # TODO
-    # NB: Data cleaning should be done separately
-    # For the selected variable, create bins of databins that only vary in that variable
-    # Graph each bin
 
     bins = databin_bins(description, args.variable)
     labels = bins_labels(bins, description, args.variable)
@@ -66,23 +76,24 @@ def main():
             per_algorithm = {}
             for algorithm in experiment["algorithm_results"]:
                 # just dump everything into one
-                collated_bins = {i: {'cycles': []} for i in subset}
+                collated_bins = {i: {cn: [] for cn in CNAMES} for i in subset}
                 for repeat in algorithm['repeat_results']:
                     databins = repeat['databin_results']
-                    for i, d in collated_bins.items():
-                        if databins[i] is None:
+                    for databin in databins:
+                        if databin is None:
                             continue
-                        for trial in databins[i]['results']['pair']:
-                            for counter in d:
-                                d[counter].extend(trial[counter][args.rw:])
+                        i = int(databin['databin_index'])
+                        collated_bin = collated_bins[i]
+                        for trial in databin['results']['pair']:
+                            for counter in CNAMES:
+                                collated_bin[counter].extend(trial[counter][args.rw:])
                 per_algorithm[algorithm['algorithm_name']] = collated_bins
             per_subset.append(per_algorithm)
         per_experiment[experiment['experiment_name']] = per_subset
 
-
     for e_name, e_data in per_experiment.items():
         for si, s_data in enumerate(e_data):
-            plot(e_name, si, s_data, args.variable, args.reference, labels[si], results_path.stem, args.width, args.height)
+            plot(e_name, si, s_data, args.variable, args.reference, labels[si], results_path.stem, args.width, args.height, note)
 
 
 def bins_labels(bins, description, variable):
@@ -156,18 +167,24 @@ def databin_bins(description: dict, variable: str) -> list[list[int]]:
     return ret
 
 
-def plot(experiment_name, subset_index, subset_data, variable_name, reference_algorithm, labels, fname_base, width, height):
+def plot(experiment_name, subset_index, subset_data, variable_name, reference_algorithm, labels, fname_base, width, height, note):
     labels = list(labels.values())
     # subset_data = {algorithm_name: collated_bins}
     #   collated_bins = {i: {'cycles': []} for i in subset}
 
     # Split reference and to-be-compared data
     ref_data = subset_data[reference_algorithm]
-    ref_avgs = {i: {c: np.average(d) for c, d in counters.items()} for i, counters in ref_data.items()}
     cmp_data = {a: d for a, d in subset_data.items() if a != reference_algorithm}
 
-    fig, ax = plt.subplots()
-    fig.set_size_inches(width, height)
+    fig = plt.figure(layout='constrained', figsize=(width, height))
+    gs = fig.add_gridspec(3, len(CNAMES), height_ratios=[10, 10, 1], hspace=0.1)
+
+    prop_axs = [fig.add_subplot(gs[0, i]) for i in range(len(CNAMES))]
+    abs_axs = [fig.add_subplot(gs[1, i]) for i in range(len(CNAMES))]
+    note_ax = fig.add_subplot(gs[2, :])
+
+    note_ax.set_axis_off()
+    note_ax.text(0, 0.5, note)
 
     for a_name in cmp_data:
         proportions = {}
@@ -179,43 +196,81 @@ def plot(experiment_name, subset_index, subset_data, variable_name, reference_al
 
             counters = collated[i]
             ref_counters = ref_data[i]
-            ref_counter_averages = ref_avgs[i]
 
-            for cname in counters:
+            for cname, propf in zip(CNAMES, PROPORTIONS):
+                values = counters[cname]
+                ref_values = ref_counters[cname]
+
+                if len(values) == 0:
+                    continue
+
+                proportions[i][cname] = propf(values, ref_values)
+                bootstraps[i][cname] = stats.bootstrap((values, ref_values), propf, n_resamples=2000)
+
+        for j, cname in enumerate(CNAMES):
+            proportion, upper, lower = [], [], []
+            for i in proportions:
+                if cname not in proportions[i]:
+                    continue
+                proportion.append(proportions[i][cname])
+                lower.append(bootstraps[i][cname].confidence_interval.low)
+                upper.append(bootstraps[i][cname].confidence_interval.high)
+
+            if len(proportion) != len(labels):
+                continue
+            prop_axs[j].plot(labels, proportion, label=a_name)
+            prop_axs[j].fill_between(labels, lower, upper, alpha=0.5)
+
+    # Configure axes
+    for i, (cname, ylabel) in enumerate(zip(CNAMES, YAXIS)):
+        prop_axs[i].set_title(f'Algorithm {cname} relative to {reference_algorithm}')
+        prop_axs[i].set_ylabel(ylabel)
+        prop_axs[i].set_xlabel(f'{variable_name.capitalize()}')
+        low, high = prop_axs[i].get_ylim()
+        prop_axs[i].set_ylim(0, max(1, high))
+        prop_axs[i].legend()
+
+    for a_name in subset_data:
+        averages = {}
+        bootstraps = {}
+
+        collated = subset_data[a_name]
+        for i in collated:
+            averages[i] = {}
+            bootstraps[i] = {}
+
+            counters = collated[i]
+            for cname in CNAMES:
                 values = counters[cname]
                 if len(values) == 0:
                     continue
-                average = np.average(values)
-                ref_values = ref_counters[cname]
-                ref_average = ref_counter_averages[cname]
+                averages[i][cname] = np.average(values)
+                bootstraps[i][cname] = stats.bootstrap((values, ), np.average, n_resamples=2000)
 
-                proportions[i][cname] = ref_average / average
-                def proportion(values, ref_values):
-                    return np.average(ref_values) / np.average(values)
-                bootstraps[i][cname] = stats.bootstrap((values, ref_values), proportion, n_resamples=2000)
+        for j, cname in enumerate(CNAMES):
+            average, upper, lower = [], [], []
+            for i in averages:
+                if cname not in averages[i]:
+                    continue
+                average.append(averages[i][cname])
+                lower.append(bootstraps[i][cname].confidence_interval.low)
+                upper.append(bootstraps[i][cname].confidence_interval.high)
 
-        proportion, upper, lower = [], [], []
-        for i in proportions:
-            if 'cycles' not in proportions[i]:
+            if len(proportion) != len(labels):
                 continue
-            proportion.append(proportions[i]['cycles'])
-            lower.append(bootstraps[i]['cycles'].confidence_interval.low)
-            upper.append(bootstraps[i]['cycles'].confidence_interval.high)
-
-        if len(proportion) != len(labels):
-            continue
-        ax.plot(labels, proportion, label=a_name)
-        ax.fill_between(labels, lower, upper, alpha=0.5)
+            abs_axs[j].plot(labels, average, label=a_name)
+            abs_axs[j].fill_between(labels, lower, upper, alpha=0.5)
 
     # Plot data
-    ax.set_title(f'Algorithm speedup relative to {reference_algorithm}')
-    ax.set_ylabel('Speedup (ref. cycles / algo. cycles)')
-    ax.set_xlabel(f'{variable_name.capitalize()}')
-    low, high = ax.get_ylim()
-    ax.set_ylim(0, max(1, high))
-    ax.legend()
+    for i, cname in enumerate(CNAMES):
+        abs_axs[i].set_title(f'Algorithm {cname}')
+        abs_axs[i].set_ylabel('Counts')
+        abs_axs[i].set_xlabel(f'{variable_name.capitalize()}')
+        low, high = abs_axs[i].get_ylim()
+        abs_axs[i].set_ylim(0, max(1, high))
+        abs_axs[i].legend()
 
-    filename = f'{fname_base}.{experiment_name}.{subset_index}.{variable_name}.cycles.png'
+    filename = f'{fname_base}.{experiment_name}.{subset_index}.{variable_name}.counters.png'
     plt.savefig(filename)
     plt.close()
 
